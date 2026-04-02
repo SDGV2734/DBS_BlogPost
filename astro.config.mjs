cae043216ccda487ea762c012734cb483a36f090
sdgv2734 <02230299.cst@rub.edu.bt>
2026-04-03 02:45:52 +0600

    deployment fix

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,8 +7,8 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://djsiddz.github.io",
-  base: "/space-ahead",
+  site: "https://dbsblogpost.netlify.app",
+  base: "/",
   integrations: [
     swup({
       theme: ["overlay", { direction: "to-top" }],
